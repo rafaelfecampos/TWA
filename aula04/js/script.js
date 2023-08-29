faf29230aa7e682d1c650fa061adcfe82459deda
Rafael Ferreira Campos <2001campos.rafael@gmail.com>
2023-08-29 20:25:24 -0300

    aula 29-09

--- aula04/js/script.js
+++ aula04/js/script.js
@@ -56,23 +56,83 @@ for (let i = 0; i< alunos.length; i++){
 // O metodo conta deve contar até o valor de numero e exibir a letra
 // da música
 
-var mariana = { //melhorar o codigo!!!!
-	numero: 10,
-	conta(){
-        for(let i = 1; i<=this.numero; i++){
+/*var mariana = { //melhorar o codigo!!!!
+    numero: 10,
+    conta() {
+        for (let i = 1; i <= this.numero; i++) {
             var conta = '';
-            for(let j = 1; j<=i ; j++){
+            for (let j = 1; j <= i; j++) {
                 conta = conta + `é ${j}, `;
             }
             console.log(`Mariana conta ${i}.`);
-            console.log(`Mariana conta ${i}: ${conta}é!`);
-            console.log(`Ana, viva a Mariana, viva a Mariana.`);
-            console.log('');
+            if (i == 1)
+                console.log(`Mariana conta 1: é 1, é 1, é!`);
+            else
+                console.log(`Mariana conta ${i}: ${conta}é!`);
+            console.log(`Ana, viva a Mariana, viva a Mariana.\n\n`);
         }
-	}
+    }
 }
 
-mariana.conta();
+mariana.conta();*/
 
-
+//Resolução Gildo
+/*var mariana = {
+    quantidade: 10,
+    conta(){
+        var letra = '';
+        for (let i = 1; i < this.quantidade; i++) {
+            letra+= `Mariana consta ${i}\n` + `Mariana conta ${i}:`;
+            for (let j = 1; j <= i ; j++) {
+                letra+= `é ${j}, `;
+            }
+            letra += `é!\n`;
+            letra+= `Ana, viva a Mariana, viva a Mariana\n\n`            
+        }
+        console.log(letra);
+    }
+}
+mariana.conta();*/
+//Utilizando .toString()
+/*var mariana = {
+    quantidade: 10,
+    conta() {
+        var letra = '';
+        var contagem = [];
+        for (let i = 1; i <= this.quantidade; i++) {
+            contagem.push(` é ${i}`);
+            letra += `Mariana consta ${i}\n` + `Mariana conta ${i}:`;
+            letra+= contagem;
+            letra += `, é!\n`;
+            letra += `Ana, viva a Mariana, viva a Mariana\n\n`
+        }
+        console.log(letra);
+    }
+}
+console.log(mariana.conta());*/
 // https://www.galinhapintadinha.com.br/musicas/letras/dvd-galinha-pintadinha-vol-1/mariana/
+
+//While
+/*var dados = [1,2,3,4,5,6]
+console.log(dados);
+while (dados.length>0){
+    dados.pop();
+    console.log(dados);
+}*/
+
+//Do while
+/*var dados = [1,2,3,4,5,6]
+console.log(dados);
+var i = 6
+do{
+    dados.pop();
+    console.log(dados);
+} while (i>10)*/
+
+//forEach
+
+var dados = [1,2,3,4,5,6]
+function ImprimirItem(item){
+    console.log(item);
+}
+dados.forEach(ImprimirItem)
