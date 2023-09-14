102774ebc949e43ed3d291590956d620984c0880
Rafael Ferreira Campos <2001campos.rafael@gmail.com>
2023-09-14 20:19:39 -0300

    aula 14-09

---
+++ aula07/js/script.js
@@ -0,0 +1,46 @@
+// let paragrafo1 = document.getElementsByClassName('impar'); //selecionar um unico elemento: getElementByID
+
+// // let arrayPar1 = Array.from(paragrafo1);
+
+// let paragrafo2 = document.querySelectorAll('.impar'); //selecionar uma classe ou elementos mais complexos
+
+// console.log(paragrafo1);
+// console.log(paragrafo2);
+
+
+//##alterar, adicionar e remover classes, ids no html
+// let paragrafo = document.querySelector('p');
+// console.log(paragrafo.className);
+const corpo = document.querySelector('body');
+const linha = document.getElementById("modoEscuro");
+
+function modoEscuro() {
+
+    if (corpo.classList.contains('escuro')) {
+        corpo.classList.remove('escuro')
+        linha.innerText = "Modo Escuro";
+    }
+    else {
+        corpo.classList.add('escuro');
+        linha.innerText = "Modo Claro";
+    }
+}
+
+function alterarModo() {
+    const textosAzul = document.querySelectorAll(".texto-azul");
+    const textosVermelho = document.querySelectorAll('.texto-vermelho')
+    if (textosAzul.length > 0) {
+        textosAzul.forEach(texto => {
+            texto.classList.replace('texto-azul', 'texto-vermelho')
+        });
+    }
+    if (textosVermelho.length > 0) {
+        textosVermelho.forEach(texto => {
+            texto.classList.replace('texto-vermelho', 'texto-azul')
+        });
+    }
+
+
+
+    corpo.classList.toggle('escuro'); //toggle simplifica a função acima, se tiver a classe x, remove x; se nao tiver a classe x, adiciona x
+}
