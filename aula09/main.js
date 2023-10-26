7059f932003117628992e8290691f7f905e54f72
Rafael Ferreira Campos <2001campos.rafael@gmail.com>
2023-10-26 20:52:44 -0300

    aula 09 26/10

---
+++ aula09/main.js
@@ -0,0 +1,59 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+//Math.random retorna um num aleatório entre 0 e 1
+//mas quero entre 0 e 255 => faz porcentagem
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+  ctx.fill();
+}
+Ball.prototype.update = function () {
+  if((this.x+this.size)>=width){
+    this.velX=-(this.velX);
+  }if((this.x+this.size)<=0){
+    this.velX=-(this.velX);
+  }if((this.y+this.size)>=height){
+    this.velY=-(this.velY);
+  }if((this.y+this.size)<=0){
+    this.velY=-(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+// let testBall = new Ball(275,150 , 80, 80, 'green', 30);
+
+// testBall.x
+// testBall.size
+// testBall.color
+// testBall.draw()
